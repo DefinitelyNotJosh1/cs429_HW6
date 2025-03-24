2f8c499962602b1cfdb5f3a2b9d98a9f66990dbf
DefinitelyNotJosh1 <157744625+DefinitelyNotJosh1@users.noreply.github.com>
2025-03-24 11:25:50 -0700

    work update 3/24/2025

--- HW6.py
+++ HW6.py
@@ -6,30 +6,71 @@
 
 import numpy as np
 
+class NaiveBayesClassifier:
+    def __init__(self):
+        self.prior = None
+        self.likelihood = None
+        self.posterior = None
+    
+    def train(self, data, labels):
+        # get the number of spam and non-spam emails
+        spam = np.sum(labels == 1)
+        non_spam = np.sum(labels == -1)
+        
+        # get the prior probability
+        self.prior = spam / (spam + non_spam)
+        
+        # get the likelihood
+        self.likelihood = np.zeros((2, data.shape[1]))
+        for i in range(data.shape[1]):
+            self.likelihood[0, i] = np.sum(data[labels == 1, i]) / spam
+            self.likelihood[1, i] = np.sum(data[labels == -1, i]) / non_spam
+
+    def predict(self, data):
+        self.posterior = np.zeros((data.shape[0], 2))
+        for i in range(data.shape[0]):
+            self.posterior[i, 0] = np.prod(data[i] * self.likelihood[0]) * self.prior
+            self.posterior[i, 1] = np.prod(data[i] * self.likelihood[1]) * (1 - self.prior)
+
+        return np.argmax(self.posterior, axis=1)
+    
+    def accuracy(self, labels):
+        return np.sum(labels == self.predicted_labels) / labels.shape[0]
+    
+
+
+# Load the data
 data = np.loadtxt('SpamInstances.txt', delimiter=' ', skiprows=1)
 
 # Split the data into training and testing sets
 np.random.shuffle(data)
 start = int(0.1*data.shape[0])
 end = int(0.9*data.shape[0])
+print(int(data.shape[0] * 0.2))
 
 train_data = data[start:end]
-test_data = data[-start:start]
+valid_data_ham = data[:start]
+valid_data_spam = data[end:]
 
-print(train_data.shape)
-print(test_data.shape)
 
 # get labels
-train_labels = test_data[:, 1]
-test_labels = test_data[:, 1]
+train_labels = train_data[:, 1]
+
 
 # get vector instance numbers
 train_instance = train_data[:, 0]
-test_instance = test_data[:, 0]
+valid_ham_instance = valid_data_ham[:, 0]
+valid_spam_instance = valid_data_spam[:, 0]
 
 # get the vector instances
-train_data = train_data[:, 2]
-test_data = test_data[:, 2]
+train_vector_data = train_data[:, 2]
+valid_vector_data_ham = valid_data_ham[:, 2]
+valid_vector_data_spam = valid_data_spam[:, 2]
+
+# get the vector data
+# train_vector_data = 
+
+print (vector_data)
 
 
 
